d7ea018d7f4a50dbd6fd332ed6dee3d9ea5b9e4a
DraftMan <nicovanaarsen@gmail.com>
2020-04-14 22:59:51 +0200

    Models added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-require('dotenv').config()
-const app = require('./src/app')
+require('dotenv').config();
 
-const server = app.listen(process.env.port || 3000, () => {
-    console.log(`API lancé sur http://127.0.0.1:${server.address().port}`)
-})
+const app = require('./src/app');
+const models = require('./src/models');
+
+models.database.sync().then(() => {
+    const server = app.listen(process.env.port || 3000, () => {
+        console.log(`API lancé sur http://127.0.0.1:${server.address().port}`);
+    });
+}).catch(console.error);
--- src/models/User.js
+++ src/models/User.js
@@ -1,24 +1,21 @@
-const Sequelize = require('sequelize');
+module.exports = (sequelize, Sequelize) => {
+    return sequelize.define('users', {
+        user_id: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+            allowNull: false,
+        },
+        user_name: Sequelize.STRING,
+        user_avatar: Sequelize.STRING,
+        user_token: Sequelize.TEXT,
+        user_token_refresh: Sequelize.TEXT,
+        token: Sequelize.TEXT,
+        limit: Sequelize.INTEGER,
+    }, {
+        indexes: [{
+            fields: ['user_id'],
+        }],
+        timestamps: false,
+    });
+};
 
-const Database = require('../services/PostgreSQL');
-
-const users = Database.db.define('users', {
-    user_id: {
-        type: Sequelize.STRING,
-        primaryKey: true,
-        allowNull: false
-    },
-    user_name: Sequelize.STRING,
-    user_avatar: Sequelize.STRING,
-    user_token: Sequelize.TEXT,
-    user_token_refresh: Sequelize.TEXT,
-    token: Sequelize.TEXT,
-    limit: Sequelize.INTEGER,
-}, {
-    indexes: [{
-        fields: ['user_id'],
-    }],
-    timestamps: false,
-});
-
-module.exports = users;
