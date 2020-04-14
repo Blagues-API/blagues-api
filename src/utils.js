977e59b74ce2aedc11fab4b364ddcb2c536b1600
DraftMan <nicovanaarsen@gmail.com>
2020-04-14 23:02:48 +0200

    API Token generator

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,16 @@
 const random = (items) => {
     return items[Math.floor(Math.random() * items.length)];
-}
+};
+
+const generateAPIToken = (jwt, user_id, limit) => {
+    return jwt.sign({
+        user_id,
+        limit,
+    }, process.env.jwt_encryption_web);
+};
+
 
 module.exports = {
-    random
-}
+    generateAPIToken,
+    random,
+};
