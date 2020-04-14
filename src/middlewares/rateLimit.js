d7c33b6ab681d6f39a7fae6c161a5a1081e6d932
DraftMan <nicovanaarsen@gmail.com>
2020-04-14 23:05:06 +0200

    Eslint

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,14 +1,12 @@
-const moment = require('moment');
-
 let hostsStats = {};
 
 setInterval(() => {
-    hostsStats = {}
+    hostsStats = {};
 }, 10000);
 
 module.exports = (req, res, next) => {
     const requester = String(req.headers['x-forwarded-for'] || req.connection.remoteAddress || 'unknown');
-    
+
     const limit = req.user.limit || 100;
 
     if (!hostsStats[requester]) {
@@ -17,13 +15,13 @@ module.exports = (req, res, next) => {
         hostsStats[requester] = hostsStats[requester] + 1;
     }
 
-    if (rateLimitedIps[requester] > limit) {
-      res.status(429).send({
-        status: 429,
-        error: "Too Many Requests",
-        message: `Your ip has exceeded your limit of ${limit} requests per 10 seconds. Please try again.`
-      })
+    if (hostsStats[requester] > limit) {
+        res.status(429).send({
+            status: 429,
+            error: 'Too Many Requests',
+            message: `Your ip has exceeded your limit of ${limit} requests per 10 seconds. Please try again.`,
+        });
     }
 
     next();
-}
+};
