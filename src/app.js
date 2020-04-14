7f6856020b4daeca4c1863ff0fb4453cc7693eb9
DraftMan <nicovanaarsen@gmail.com>
2020-04-14 23:00:59 +0200

    express app setup

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const cookies = require('cookies');
 // const secure = require('express-force-https')
 
-const { cors, rateLimit } = require('./middlewares');
-const routes = require('./router')
+const cors = require('./middlewares/cors');
+const routes = require('./router');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(helmet())
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cookieParser());
+app.use(cookies.express(['random key']));
 // app.use(secure);
-app.use(cors())
+app.use(cors());
+
+app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/', express.static(__dirname + '/../dist'));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-// app.use(middlewares.rateLimit)
-
-app.use('/', routes)
+app.use('/', routes);
 
 module.exports = app;
