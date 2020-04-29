c342f53a781bfec86d87099726f44276da44f6a6
saphirrc <59259156+saphirrc@users.noreply.github.com>
2020-04-29 12:56:59 -0400

    update bot

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 
 const app = require('./src/app');
-const bot = require('./src/bot');
 const models = require('./src/models');
 
-bot.login(process.env.discord_bot_token);
+require('./src/bot');
 
 models.database.sync().then(() => {
     const server = app.listen(process.env.port || 3000, () => {
--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ const { Client } = require('discord.js');
 const jokes = require('../blagues.json');
 
 const adminUsers = ['555068713343254533', '207190782673813504'];
+
 const suggestsChannel = '698826767221391390';
 
 const BlagueAPIBot = new Client();
@@ -14,21 +15,23 @@ BlagueAPIBot.on('ready', () => {
 
 BlagueAPIBot.on('messageReactionAdd', (messageReaction, user) => {
 
-    if(messageReaction.message.channel !== suggestsChannel) return;
+//    if(messageReaction.message.channel !== suggestsChannel) return;
 
     if(messageReaction.emoji.id !== '673309915988623393' || !adminUsers.includes(user.id)) {
         return messageReaction.users.remove(user);
     }
 
-    // définir la regex
+    const regex = /(?:> \*\*Type\*\*: (.\S+)(?:\s+)?)(?:> \*\*Blague\*\*: (.+)\s+)(?:> \*\*Réponse\*\*: (.+)\s+)(?:> ▬+)/gmi
 
-    // utiliser la regex pour verifier si c'est bien une blague: String.test(Regex)
+    if(!regex.test(messageReaction.message.content)) return;
 
     // recupérer les 3 groups Type, Blague, Réponse
-
+   
+    const result = regex.exec(messageReaction.message.content);
+    console.log(messageReaction.message.content)
     // Convertir le type
 
-    const type = '';
+    const type = ''; 
     const joke = '';
     const answer = '';
 
@@ -47,4 +50,4 @@ BlagueAPIBot.on('messageReactionAdd', (messageReaction, user) => {
     });
 });
 
-return BlagueAPIBot;
+BlagueAPIBot.login(process.env.discord_bot_token);
