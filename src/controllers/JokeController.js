6456e7b3f43c7d3feb3a4a50c5721f883614e4d0
DraftMan <nicovanaarsen@gmail.com>
2020-05-17 16:13:24 +0200

    Errors managing + type filter added

--- src/controllers/JokeController.js
+++ src/controllers/JokeController.js
@@ -1,20 +1,52 @@
 const jokes = require('../../blagues.json');
 const { random } = require('../utils');
 
-const randomJoke = () => {
-    return random(jokes);
+const typesRefs = {
+    'global': 'Général',
+    'dark': 'Noir',
+    'dev': 'Développeur',
+    'limit': 'Limite limite',
+    'beauf': 'Beauf',
+    'blondes': 'Blondes',
+};
+
+const randomJoke = (disallow) => {
+    const typesForbidden = Array.isArray(disallow) ? disallow : Array.of(disallow);
+    if(disallow && typesForbidden.some(type => !Object.keys(typesRefs).includes(type))) {
+        return {
+            error: true,
+            message: 'Bad type provided',
+        };
+    }
+    return {
+        error: false,
+        response: random(disallow ? jokes.filter(joke => !typesForbidden.includes(joke.type)) : jokes),
+    };
 };
 
 const jokeById = (id) => {
-    return jokes.find(joke => joke.id === id);
+    const searchedJoke = jokes.find(joke => joke.id === id);
+    return {
+        error: !!searchedJoke,
+        response: searchedJoke,
+    };
 };
 
 const randomJokeByType = (type) => {
-    return random(jokes.filter(joke => joke.type === type));
+    if(!Object.keys(typesRefs).includes(type)) {
+        return {
+            error: true,
+            message: 'Bad type provided',
+        };
+    }
+    return {
+        error: false,
+        response: random(jokes.filter(joke => joke.type === type)),
+    };
 };
 
 const jokesCount = () => {
     return jokes.length;
 };
 
-module.exports = { randomJoke, randomJokeByType, jokeById, jokesCount };
+module.exports = { randomJoke, randomJokeByType, jokeById, jokesCount, typesRefs };
