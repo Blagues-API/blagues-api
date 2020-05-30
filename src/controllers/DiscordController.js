49e486c3807c17d1f905b0532836136cb1e981c7
DraftMan <nicovanaarsen@gmail.com>
2020-05-30 21:23:28 +0200

    Auth updated

--- src/controllers/DiscordController.js
+++ src/controllers/DiscordController.js
@@ -1,5 +1,4 @@
-const fetch = require('node-fetch');
-const btoa = require('btoa');
+const axios = require('axios');
 const jwt = require('jsonwebtoken');
 
 const { Users } = require('../models');
@@ -19,48 +18,53 @@ function callback() {
         if (!req.query.code) {
             return res.status(400).json({ status: 400, error: 'Bad Request', message: 'Code query missing' });
         }
-        const authPayloadRq = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${req.query.code}&redirect_uri=${uri}`, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Basic ${btoa(`${process.env.discord_client_id}:${process.env.discord_client_secret}`)}`,
-            },
-        });
-        const authPayload = await authPayloadRq.json();
-        const userPayloadRq = await fetch('http://discordapp.com/api/users/@me', {
-            headers: {
-                'Authorization': `Bearer ${authPayload.access_token}`,
-            },
-        });
-        const userPayload = await userPayloadRq.json();
-        const user = await Users.findOne({ where: { user_id: userPayload.id }, raw: true });
 
-        if(user) {
-            await Users.update({
-                user_name: userPayload.username,
-                user_avatar: userPayload.avatar,
-                user_token: authPayload.access_token,
-                user_token_refresh: authPayload.refresh_token,
-            }, {
-                where: { user_id: userPayload.id },
+        try {
+            const { data: authPayload } = await axios.post(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&redirect_uri=${uri}`, `code=${req.query.code}`, {
+                auth: {
+                    username: process.env.discord_client_id,
+                    password: process.env.discord_client_secret,
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             });
-        } else {
-            const key = generateKey();
-            const token = await generateAPIToken(userPayload.id, key, 100);
 
-            await Users.create({
-                user_id: userPayload.id,
-                user_name: userPayload.username,
-                user_avatar: userPayload.avatar,
-                user_token: authPayload.access_token,
-                user_token_refresh: authPayload.refresh_token,
-                token_key: key,
-                token,
-                limit: 100,
+            const { data: userPayload } = await axios.get('http://discordapp.com/api/users/@me', {
+                headers: {
+                    'Authorization': `Bearer ${authPayload.access_token}`,
+                },
             });
+            const user = await Users.findOne({ where: { user_id: userPayload.id } });
+
+            if(user) {
+                await Users.update({
+                    user_name: userPayload.username,
+                    user_avatar: userPayload.avatar,
+                    user_token: authPayload.access_token,
+                    user_token_refresh: authPayload.refresh_token,
+                }, {
+                    where: { user_id: userPayload.id },
+                });
+            } else {
+                const key = generateKey();
+                const token = await generateAPIToken(userPayload.id, key, 100);
+
+                await Users.create({
+                    user_id: userPayload.id,
+                    user_name: userPayload.username,
+                    user_avatar: userPayload.avatar,
+                    user_token: authPayload.access_token,
+                    user_token_refresh: authPayload.refresh_token,
+                    token_key: key,
+                    token,
+                    limit: 100,
+                });
+            }
+
+            const key = await jwt.sign(authPayload.access_token, process.env.jwt_encryption_web);
+            res.cookies.set('auth', key);
+        } catch (error) {
+            console.error('Discord-Auth', error);
         }
-
-        const key = await jwt.sign(authPayload.access_token, process.env.jwt_encryption_web);
-        res.cookies.set('auth', key);
 
         return res.redirect('/account');
     };
--- src/middlewares/auth-web.js
+++ src/middlewares/auth-web.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 const { Users } = require('../models');
 
@@ -9,13 +9,13 @@ module.exports = function() {
 
         if(auth) {
             const token = await jwt.verify(auth, process.env.jwt_encryption_web);
-            const userRq = await fetch('http://discordapp.com/api/users/@me', {
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
-            const decoded = await userRq.json();
-            req.user = await Users.findOne({ where: { user_id: decoded.id }, raw: true });
+            try {
+                const { data } = await axios.get('http://discordapp.com/api/users/@me', { headers: { 'Authorization': `Bearer ${token}` } });
+                const user = await Users.findOne({ where: { user_id: data.id }, raw: true });
+                req.user = user;
+            } catch (error) {
+                console.error('Auth-Web:', error);
+            }
         }
         next();
     };
