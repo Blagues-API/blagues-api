0f1412c2208a17fc9b3d4205ad67c35a77603c37
DraftProducts <nicovanaarsen@gmail.com>
2020-11-13 21:30:34 +0100

    style: :art: Update linters & formaters: eslint, prettier, stylelint, commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    'standard',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'vue/no-v-html': 'off',
+    curly: [2, 'multi-line'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'arrow-parens': ['error', 'as-needed'],
+  },
+}
---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'color-hex-length': 'long',
+    'block-no-empty': [
+      true,
+      {
+        severity: 'warning',
+      },
+    ],
+  },
+}
