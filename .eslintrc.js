8ff923dc4c5462137956f2f69bad805f8761d3eb
DraftProducts <nicovanaarsen@gmail.com>
2020-11-13 21:35:51 +0100

    chore: :wrench: Fix lint of eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'standard',
     'prettier',
     'prettier/vue',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   plugins: ['prettier'],
   // add your custom rules here
