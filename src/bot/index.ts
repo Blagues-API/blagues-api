eb4db9d364413defe7e433339123de50ec73c79a
DraftProducts <nicovanaarsen@gmail.com>
2021-07-12 22:46:38 +0200

    feat(bot): ✨ Create initial bot code
    Co-authored-by: ErwanGit <24718500+ErwanGit@users.noreply.github.com>

---
+++ src/bot/index.ts
@@ -0,0 +1,37 @@
+import { Client, CommandInteraction, Intents, Interaction } from 'discord.js';
+import jokes from '../../blagues.json';
+// import { AdminUsers, jokeRole, suggestsChannel, logsChannel} from './constents'
+import suggestCommand from './commands/suggest';
+
+const BlaguesAPIBot = new Client({
+  partials: ['REACTION'],
+  intents: Intents.FLAGS.GUILDS
+});
+
+BlaguesAPIBot.on('ready', () => {
+  console.log(`${BlaguesAPIBot.user!.tag} connecté !`);
+
+  BlaguesAPIBot.user!.setActivity(`les ${jokes.length} blagues`, {
+    type: 'WATCHING'
+  });
+  setInterval(() => {
+    BlaguesAPIBot.user!.setActivity(`les ${jokes.length} blagues`, {
+      type: 'WATCHING'
+    });
+  }, 24 * 60 * 60 * 1000);
+});
+
+BlaguesAPIBot.on('interactionCreate', async (interaction: Interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command: CommandInteraction = interaction as CommandInteraction;
+
+  switch (command.commandName) {
+    case 'suggest':
+      return suggestCommand(command);
+    default:
+      break;
+  }
+});
+
+BlaguesAPIBot.login(process.env.discord_bot_token);
