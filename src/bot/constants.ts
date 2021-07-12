a8e713b7ffa2a9b92bc56b3f0b1e8a432c1cba81
DraftProducts <nicovanaarsen@gmail.com>
2021-07-12 22:47:01 +0200

    feat(bot): ✨ Create bot constants
    Co-authored-by: ErwanGit <24718500+ErwanGit@users.noreply.github.com>

---
+++ src/bot/constants.ts
@@ -0,0 +1,11 @@
+export enum AdminUsers {
+  DraftMan = "207190782673813504",
+  Saphirr = "207190782673813504",
+  Panda = "164738865649811457",
+  Esther = "655032713941614632",
+  Erwan = "150249602635792385"
+}
+
+export const jokeRole: `${bigint}` = '699244416849674310';
+export const suggestsChannel: `${bigint}` = '698826767221391390';
+export const logsChannel: `${bigint}` = '763778635857133599';
