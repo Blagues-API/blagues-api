4cb37f440757aa64f20adeebfb749d1fc104e44a
DraftProducts <nicovanaarsen@gmail.com>
2021-07-12 22:48:29 +0200

    feat: ✨ Create web app with fastify and plugins

---
+++ src/app/index.ts
@@ -0,0 +1,21 @@
+import fastify, { FastifyInstance } from 'fastify';
+import api from './api';
+import nuxt from './nuxt';
+
+export default async () => {
+  const app: FastifyInstance = fastify();
+
+  try {
+    await app.register(api);
+    await app.register(nuxt);
+
+    await app.listen(3000);
+
+    console.log(`
+      🚀 Blagues API lancé: http://localhost:3000
+    `);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
