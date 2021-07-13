e071d95806817175727bec2ae8548ce38a454f31
DraftProducts <nicovanaarsen@gmail.com>
2021-07-14 00:39:13 +0200

    refactor(ts): ✏️ Remove typescript from Nuxt

--- nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-import { NuxtConfig } from '@nuxt/types';
-
-const config: NuxtConfig = {
-  srcDir: 'src/app/nuxt',
-  buildModules: ['@nuxt/typescript-build']
-};
-
-export default config;
--- src/app/nuxt/pages/index.vue
+++ src/app/nuxt/pages/index.vue
@@ -1,52 +1,18 @@
 <template>
-  <section class="hero">
-    <div class="joke" v-if="joke">
-      <div class="content">
-        <span id="joke">{{ joke.joke }}</span
-        ><br />
-        <span id="answer">{{ joke.answer }}</span>
-      </div>
-      <div class="footer">
-        <div class="joke_type" id="type">
-          {{ jokesTypes[joke.type] }}
-        </div>
-        <button id="next" @click="fetchJoke()">UNE AUTRE !</button>
-      </div>
-    </div>
-  </section>
+  <div class="container">
+    <JokeDisplay />
+    <Informations />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, onMounted } from '@vue/composition-api';
+<script>
+import Informations from '../components/home/Informations';
+import JokeDisplay from '../components/home/JokeDisplay';
 
-// import jokes from '../../../../blagues.json';
-
-const jokesTypes = {
-  limit: 'Blague limite limite',
-  global: 'Blague normale',
-  dark: 'Blague humour noir',
-  dev: 'Blague de développeurs',
-  beauf: 'Humour de beaufs',
-  blondes: 'Blagues blondes'
-};
-
-export default defineComponent({
-  setup() {
-    const joke = ref(null);
-
-    const fetchJoke = async () => {
-      // joke.value = jokes[Math.floor(Math.random() * jokes.length)];
-    };
-
-    onMounted(() => {
-      fetchJoke();
-    });
-
-    return {
-      joke,
-      fetchJoke,
-      jokesTypes
-    };
+export default {
+  components: {
+    Informations,
+    JokeDisplay
   }
-});
+};
 </script>
