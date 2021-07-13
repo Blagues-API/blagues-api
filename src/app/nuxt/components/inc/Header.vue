38dbd3309b9ff51b2c7a1b95c3392b158d696677
DraftProducts <nicovanaarsen@gmail.com>
2021-07-14 00:44:32 +0200

    feat(nuxt): ✨ Create header component

---
+++ src/app/nuxt/components/inc/Header.vue
@@ -0,0 +1,144 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <a class="brand" href="/" title="Accueil">
+        <img class="logo" src="/logo.svg" alt="Logo Blagues API" />
+        <h1 class="name">BLAGUES API</h1>
+      </a>
+      <div class="navigation">
+        <a
+          class="item"
+          href="https://github.com/DraftProducts/blagues-api"
+          title="Github de Blagues API"
+          >GITHUB</a
+        >
+        <a
+          class="item"
+          href="https://discord.gg/PPNpVaF"
+          title="Discord de Blagues API"
+          >DISCORD</a
+        >
+        <a class="user-place" v-if="auth" href="/account">
+          <div
+            class="avatar"
+            :style="{
+              'background-image': `url('https://cdn.discordapp.com/avatars/${auth.user.id}/${auth.user.avatar}.png?size=64')`
+            }"
+          ></div>
+          <span class="username">{{ auth.user.username }}</span>
+        </a>
+        <nuxt-link
+          class="item rounded"
+          v-else
+          to="/login"
+          title="Connexion Discord"
+          >CONNEXION</nuxt-link
+        >
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--matisse);
+  color: var(--white);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.5);
+  .wrapper {
+    width: 1110px;
+    max-width: 90%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    @media (max-width: 720px) {
+      flex-direction: column;
+    }
+    .brand {
+      display: flex;
+      align-items: center;
+      width: 60%;
+      margin-right: 8%;
+      user-select: none;
+      cursor: pointer;
+      color: var(--white);
+      text-decoration: none;
+      @media (max-width: 720px) {
+        width: 100%;
+        justify-content: center;
+        margin-right: 0;
+      }
+
+      .logo {
+        width: 70px;
+        height: 70px;
+      }
+
+      .name {
+        margin-left: 6px;
+        font-size: 26px;
+        font-weight: 800;
+        text-shadow: 1px 1px 3px #000000cc;
+      }
+    }
+    .navigation {
+      display: flex;
+      align-items: center;
+
+      @media (max-width: 720px) {
+        width: 100%;
+        justify-content: center;
+        padding: 12px;
+      }
+
+      .item {
+        font-weight: 600;
+        margin: 10px;
+        text-decoration: none;
+        color: white;
+        &.rounded {
+          display: flex;
+          padding: 10px 14px;
+          background-color: var(--white);
+          color: var(--matisse);
+          border-radius: 18px;
+        }
+        @media (max-width: 420px) {
+          margin: 10px 5px;
+        }
+      }
+      .user-place {
+        display: flex;
+        align-items: center;
+        margin: 0 10px;
+        @media (max-width: 420px) {
+          margin: 0 5px;
+        }
+        .avatar {
+          background-size: 32px;
+          height: 36px;
+          width: 36px;
+          border-radius: 18px;
+          border: 2px solid var(--white);
+          @media (max-width: 420px) {
+            display: none;
+          }
+        }
+        .username {
+          margin-left: 8px;
+          text-transform: none;
+          font-weight: 600;
+          font-size: 18px;
+          color: white;
+          @media (max-width: 420px) {
+            font-size: 16px;
+            margin-left: 0;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
