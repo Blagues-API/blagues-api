90155fdb56cb4d9a458826de91954d127c88d97c
DraftProducts <nicovanaarsen@gmail.com>
2021-07-14 00:43:30 +0200

    feat(nuxt): ✨ Create footer component

---
+++ src/app/nuxt/components/inc/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <div class="copyright">
+      Blagues API © 2019-2021
+      <a href="https://www.draftman.fr" title="DraftMan Developer">DraftMan</a>
+      | Powered by
+      <a href="https://www.draftbot.fr" title="DraftBot Discord Bot"
+        >DraftBot</a
+      >
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--bluewood);
+  color: var(--white);
+  height: 40px;
+  .copyright {
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 22px;
+  }
+}
+</style>
