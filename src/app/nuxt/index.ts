e83668c2328437c062fed886afa7353afd454678
DraftProducts <nicovanaarsen@gmail.com>
2021-09-03 12:20:52 +0200

    fix: 🐛 add formbody to try to solve missing content-type

--- src/app/nuxt/index.ts
+++ src/app/nuxt/index.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import nuxtPlugin from 'fastify-nuxtjs';
+import formbodyPlugin from 'fastify-formbody';
 
 export default async function (fastify: FastifyInstance) {
+  await fastify.register(formbodyPlugin);
+
   // Register Nuxt Plugin
   await fastify.register(nuxtPlugin);
 
