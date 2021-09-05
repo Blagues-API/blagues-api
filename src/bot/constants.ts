34955e0288610bb489d8a1cbb96433b3eb2787ef
DraftProducts <nicovanaarsen@gmail.com>
2021-09-05 16:02:47 +0200

    feat: ✨ add guildId constant

--- src/bot/constants.ts
+++ src/bot/constants.ts
@@ -1,3 +1,5 @@
+import { Snowflake } from 'discord-api-types';
+
 export enum AdminUsers {
   DraftMan = '207190782673813504',
   Saphirr = '207190782673813504',
@@ -6,8 +8,9 @@ export enum AdminUsers {
   Erwan = '150249602635792385'
 }
 
-export const jokeRole: `${bigint}` = '699244416849674310';
-export const parrainRole: `${bigint}` = '877511831525154837';
-export const everyoneRole: `${bigint}` = '877545109426683904';
-export const suggestsChannel: `${bigint}` = '875680955627630652';
-export const correctionChannel: `${bigint}` = '875680971683414026';
+export const jokeRole: Snowflake = '699244416849674310';
+export const parrainRole: Snowflake = '877511831525154837';
+export const everyoneRole: Snowflake = '877545109426683904';
+export const suggestsChannel: Snowflake = '875680955627630652';
+export const correctionChannel: Snowflake = '875680971683414026';
+export const guildId: Snowflake = '698822532467523605';
