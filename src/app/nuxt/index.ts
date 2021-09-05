79e479f0be41405fab6b903329ba9b1c4ba8ceb8
DraftProducts <nicovanaarsen@gmail.com>
2021-09-05 02:04:58 +0200

    fix: 🐛 new try to debug nuxt programatic

--- src/app/nuxt/index.ts
+++ src/app/nuxt/index.ts
@@ -6,5 +6,8 @@ export default async function (fastify: FastifyInstance) {
   // Register Nuxt Plugin
   await fastify.register(nuxtPlugin);
 
+  // @ts-ignore asking schema
+  fastify.nuxt('/_auth/*', { method: 'POST' });
+
   fastify.nuxt('*');
 };
