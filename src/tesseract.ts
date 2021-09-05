31a64e72b931a3bd24b813845994d067175824d1
DraftProducts <nicovanaarsen@gmail.com>
2021-09-05 13:43:17 +0200

    build: 🔨 add nuxt as extern process

--- src/tesseract.ts
+++ src/tesseract.ts
@@ -1,19 +1,23 @@
-import App from './app';
+import Api from './api';
 import Bot from './bot';
+import Nuxt from './nuxt';
 
 export default class Tesseract {
   public bot: Bot;
-  public app: App;
+  public api: Api;
+  public nuxt: Nuxt;
 
   constructor() {
     this.bot = new Bot();
-    this.app = new App();
+    this.api = new Api();
+    this.nuxt = new Nuxt();
 
     this.init();
   }
 
   async init(): Promise<void> {
-    await this.app.start();
+    await this.api.start();
+    await this.nuxt.start();
     await this.bot.start();
   }
 }
