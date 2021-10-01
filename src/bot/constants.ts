c81335dc72ee3456f4e2f1f748a64f2cc636dc50
DraftProducts <nicovanaarsen@gmail.com>
2021-10-01 12:29:54 +0200

    refactor: ♻️ Update suggestsChannel on test channel

--- src/bot/constants.ts
+++ src/bot/constants.ts
@@ -11,6 +11,6 @@ export enum AdminUsers {
 export const jokeRole: Snowflake = '699244416849674310';
 export const parrainRole: Snowflake = '877511831525154837';
 export const everyoneRole: Snowflake = '877545109426683904';
-export const suggestsChannel: Snowflake = '875680955627630652';
+export const suggestsChannel: Snowflake = '884077276210290789';
 export const correctionChannel: Snowflake = '875680971683414026';
 export const guildId: Snowflake = '698822532467523605';
