c247b71efa5e1f0bde622de66c9b94d292657d6c
DraftProducts <nicovanaarsen@gmail.com>
2021-10-01 12:29:20 +0200

    refactor: 🏷️ Add some new typings

--- src/typings.ts
+++ src/typings.ts
@@ -5,11 +5,9 @@ export interface Joke {
   answer: string;
 }
 
-export type JokeKey =
-  | 'id'
-  | 'type'
-  | 'joke'
-  | 'answer'
+export type UnsignedJoke = Omit<Joke, 'id'>;
+
+export type JokeKey = 'id' | 'type' | 'joke' | 'answer';
 
 export interface JokeNotPublished {
   message_id: string;
@@ -18,11 +16,7 @@ export interface JokeNotPublished {
   answer: string;
 }
 
-export type JokeNotPublishedKey =
-  | 'message_id'
-  | 'type'
-  | 'joke'
-  | 'answer'
+export type JokeNotPublishedKey = 'message_id' | 'type' | 'joke' | 'answer';
 
 export enum Categories {
   GLOBAL = 'global',
@@ -34,7 +28,8 @@ export enum Categories {
 }
 
 export const JokeTypes = ['global', 'dev', 'dark', 'limit', 'beauf', 'blondes'];
-export const JokeTypesRefs = {
+
+export const JokeTypesRefs: Record<Category, CategoryName> = {
   global: 'Général',
   dark: 'Noir',
   dev: 'Développeur',
@@ -42,6 +37,16 @@ export const JokeTypesRefs = {
   beauf: 'Beauf',
   blondes: 'Blondes'
 };
+
+export const JokeTypesRefsReverse: Record<CategoryName, Category> = {
+  Général: 'global',
+  Noir: 'dark',
+  Développeur: 'dev',
+  '18+': 'limit',
+  Beauf: 'beauf',
+  Blondes: 'blondes'
+};
+
 export const JokeTypesDescriptions = {
   global: 'Blagues tout public, accessibles pour tous.',
   dark: 'Blagues qui souligne avec cruauté certains faits.',
@@ -58,6 +63,14 @@ export type Category =
   | 'limit'
   | 'beauf'
   | 'blondes';
+
+export type CategoryName =
+  | 'Général'
+  | 'Noir'
+  | 'Développeur'
+  | '18+'
+  | 'Beauf'
+  | 'Blondes';
 
 /*
 See api src/app/api/index.ts file for more informations
