6825a732afe4b6831f7df08cc4984cd0702b7f57
DraftProducts <nicovanaarsen@gmail.com>
2021-10-11 01:27:53 +0200

    feat: ✨ Add parrain permission system + multiple commands type system

--- src/bot/lib/command.ts
+++ src/bot/lib/command.ts
@@ -1,17 +1,60 @@
-import { ApplicationCommandData, CommandInteraction } from 'discord.js';
+import {
+  ApplicationCommandPermissions,
+  ChatInputApplicationCommandData,
+  CommandInteraction,
+  MessageApplicationCommandData
+} from 'discord.js';
+import { parrainRole } from '../constants';
+
+interface CommandChatInfos extends ChatInputApplicationCommandData {
+  parrainOnly?: boolean;
+}
+
+interface CommandMessageInfos extends MessageApplicationCommandData {
+  parrainOnly?: boolean;
+}
 
 export default class Command {
   public name: string;
 
-  private raw: ApplicationCommandData;
+  private raw: CommandChatInfos | CommandMessageInfos;
 
-  constructor(data: ApplicationCommandData) {
+  public parrainOnly: boolean;
+
+  constructor(data: CommandChatInfos | CommandMessageInfos) {
     this.name = data.name;
     this.raw = data;
+
+    this.parrainOnly = data.parrainOnly ?? false;
   }
 
-  public get data(): ApplicationCommandData {
-    return this.raw;
+  public get data():
+    | ChatInputApplicationCommandData
+    | MessageApplicationCommandData {
+    if (this.raw.type === 'CHAT_INPUT') {
+      return {
+        name: this.name,
+        description: this.raw.description,
+        options: this.raw.options,
+        defaultPermission: !this.raw.parrainOnly
+      } as ChatInputApplicationCommandData;
+    }
+
+    return {
+      name: this.name,
+      defaultPermission: !this.raw.parrainOnly
+    } as MessageApplicationCommandData;
+  }
+
+  public get permissions(): ApplicationCommandPermissions[] | null {
+    if (!this.raw.parrainOnly) return null;
+    return [
+      {
+        id: parrainRole,
+        type: 'ROLE',
+        permission: true
+      }
+    ];
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
