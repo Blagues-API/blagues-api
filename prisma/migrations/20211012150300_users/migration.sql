2411cf3d2a2cbcc52061dda6fd6156d5bab84982
DraftProducts <nicovanaarsen@gmail.com>
2021-10-12 19:45:24 +0200

    build: 🔨 Move sqlite to postgres

--- prisma/migrations/20211012150300_users/migration.sql
+++ prisma/migrations/20211012150300_users/migration.sql
@@ -1,13 +1,15 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "user_id" TEXT NOT NULL PRIMARY KEY,
-    "user_name" TEXT NOT NULL,
+    "user_id" VARCHAR(255) NOT NULL,
+    "user_name" VARCHAR(255) NOT NULL,
     "user_avatar" TEXT NOT NULL,
     "user_token" TEXT NOT NULL,
     "user_token_refresh" TEXT NOT NULL,
     "token" TEXT NOT NULL,
     "token_key" TEXT NOT NULL,
-    "limit" INTEGER NOT NULL DEFAULT 100
+    "limit" INTEGER NOT NULL DEFAULT 100,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
 );
 
 -- CreateIndex
