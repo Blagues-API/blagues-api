c67b551c82d612942b6a40637d823a390297f736
DraftProducts <nicovanaarsen@gmail.com>
2021-10-12 20:11:56 +0200

    feat: ✨ Add suggest system

--- src/bot/commands/suggest.ts
+++ src/bot/commands/suggest.ts
@@ -1,8 +1,10 @@
 import { stripIndents } from 'common-tags';
 import {
+  ButtonInteraction,
   ColorResolvable,
   CommandInteraction,
-  MessageActionRow,
+  Message,
+  MessageOptions,
   MessageButton,
   MessageComponentInteraction,
   MessageEmbedOptions,
@@ -10,10 +12,13 @@ import {
 } from 'discord.js';
 import { findBestMatch } from 'string-similarity';
 import jokes from '../../../blagues.json';
-import { Category, JokeTypesRefs } from '../../typings';
-import { suggestsChannel } from '../constants';
+import { Category, CategoriesRefs, UnsignedJoke } from '../../typings';
+import { downReaction, suggestsChannel, upReaction } from '../constants';
 import Command from '../lib/command';
 import { interactionError } from '../utils';
+import Collection from '@discordjs/collection';
+import prisma from '../../prisma';
+import { ProposalType } from '.prisma/client';
 
 enum Similarity {
   Different,
@@ -38,7 +43,7 @@ export default class SuggestCommand extends Command {
           name: 'type',
           description: 'Général, Développeur, Noir, +18, Beauf, Blondes',
           required: true,
-          choices: Object.entries(JokeTypesRefs).map(([key, name]) => ({
+          choices: Object.entries(CategoriesRefs).map(([key, name]) => ({
             name,
             value: key
           }))
@@ -79,16 +84,25 @@ export default class SuggestCommand extends Command {
       jokes.map((e) => `${e.joke} ${e.answer}`)
     );
 
-    let similarity: Similarity = Similarity.Different;
-    if (bestMatch.rating > 0.6)
-      similarity = bestMatch.rating > 0.8 ? Similarity.Same : Similarity.Like;
+    // TODO: Compare with saved updates
+
+    const similarity =
+      bestMatch.rating > 0.6
+        ? bestMatch.rating > 0.8
+          ? Similarity.Same
+          : Similarity.Like
+        : Similarity.Different;
+
+    const payload = {
+      type: interaction.options.get('type')!.value as Category,
+      joke: interaction.options.get('joke')!.value,
+      answer: interaction.options.get('response')!.value
+    } as UnsignedJoke;
 
     let description = stripIndents`
-      > **Type**: ${
-        JokeTypesRefs[interaction.options.get('type')!.value as Category]
-      }
-      > **Blague**: ${interaction.options.get('joke')!.value}
-      > **Réponse**: ${interaction.options.get('response')!.value}
+      > **Type**: ${CategoriesRefs[payload.type]}
+      > **Blague**: ${payload.joke}
+      > **Réponse**: ${payload.answer}
     `;
 
     if (similarity !== Similarity.Different) {
@@ -98,7 +112,7 @@ export default class SuggestCommand extends Command {
         **[Blague similaire](https://github.com/Blagues-API/blagues-api/blob/master/blagues.json#L${
           6 * jokes[bestMatchIndex].id - 4
         }-L${6 * jokes[bestMatchIndex].id + 1})**
-        >>> **Type**: ${JokeTypesRefs[jokes[bestMatchIndex].type as Category]}
+        >>> **Type**: ${CategoriesRefs[jokes[bestMatchIndex].type as Category]}
         **Blague**: ${jokes[bestMatchIndex].joke}
         **Réponse**: ${jokes[bestMatchIndex].answer}
       `;
@@ -129,72 +143,117 @@ export default class SuggestCommand extends Command {
       timestamp: new Date()
     };
 
-    const row = new MessageActionRow({
-      components: [
-        new MessageButton({
-          label: 'Envoyer',
-          customId: 'true',
-          style: 'SUCCESS'
-        }),
-        new MessageButton({
-          label: 'Annuler',
-          customId: 'false',
-          style: 'DANGER'
-        })
-      ]
-    });
-
-    await interaction.reply({
-      content:
-        similarity === Similarity.Same
-          ? 'Cette blague existe déjà.'
-          : `Votre blague est sur le point d'être ajouté aux suggestions de blagues, veuillez confirmer la suggestion si vous êtes sûr qu'elle ne contient pas de fautes.\n\n
-          ${
-            similarity === Similarity.Like
-              ? "⚠️ Attention, une blague similaire existe déjà, êtes vous sûr qu'elle est différente ?"
-              : ''
-          }
-        `,
-      embeds: [embed],
-      components: similarity !== Similarity.Same ? [row] : [],
-      ephemeral: true
-    });
-
-    const confirmation = await interaction
-      .channel!.awaitMessageComponent({
-        filter: (i: MessageComponentInteraction) =>
-          i.user.id === interaction.user.id,
-        time: 30000
-      })
-      .catch(() => null);
-
-    switch (confirmation?.customId) {
-      case 'true': {
-        const channel: TextChannel = interaction.guild!.channels.cache.get(
-          suggestsChannel
-        ) as TextChannel;
-
-        await channel.send({ embeds: [embed] });
-
-        /**
-         * TODO: Ajouter les réactions
-         */
-
-        return confirmation.update({
-          content: 'La blague à été envoyée',
-          components: [],
-          embeds: []
-        });
-      }
-      case 'false': {
-        return confirmation.update({
-          content: "La blague n'a pas été envoyé",
-          components: [],
-          embeds: [embed]
-        });
-      }
-      default:
-        return;
+    if (similarity === Similarity.Same) {
+      return interaction.reply({
+        content: 'Cette blague existe déjà.',
+        embeds: [embed],
+        ephemeral: true
+      });
     }
+
+    const message = {
+      content: [
+        "Votre blague est sur le point d'être ajouté aux suggestions de blagues, veuillez confirmer la suggestion si vous êtes sûr qu'elle ne contient pas de fautes.",
+        similarity === Similarity.Like &&
+          "⚠️ Attention, une blague qui y ressemblant existe déjà, êtes vous sûr qu'elle est différente ?"
+      ]
+        .filter((l) => l)
+        .join('\n\n'),
+      embeds: [embed]
+    };
+
+    const confirmation = await this.waitForConfirmation(message, interaction);
+    if (!confirmation) return;
+
+    if (confirmation.customId === 'cancel') {
+      return confirmation.update({
+        content: "La blague n'a pas été envoyé",
+        components: [],
+        embeds: [embed]
+      });
+    }
+
+    const channel: TextChannel = interaction.guild!.channels.cache.get(
+      suggestsChannel
+    ) as TextChannel;
+
+    const suggestion = await channel.send({ embeds: [embed] });
+
+    await prisma.proposal.create({
+      data: {
+        user_id: interaction.user.id,
+        message_id: suggestion.id,
+        type: ProposalType.SUGGESTION,
+        joke_question: payload.joke,
+        joke_answer: payload.answer,
+        joke_type: payload.type
+      }
+    });
+
+    for (const reaction of [upReaction, downReaction]) {
+      await suggestion.react(reaction).catch(() => null);
+    }
+
+    return confirmation.update({
+      content: 'La blague à été envoyée',
+      components: [],
+      embeds: []
+    });
+  }
+
+  async waitForConfirmation(
+    messageData: Partial<MessageOptions>,
+    interaction: CommandInteraction
+  ): Promise<ButtonInteraction | null> {
+    const message = (await interaction.reply({
+      ...messageData,
+      components: [
+        {
+          type: 'ACTION_ROW',
+          components: [
+            new MessageButton({
+              label: 'Envoyer',
+              customId: 'send',
+              style: 'SUCCESS'
+            }),
+            new MessageButton({
+              label: 'Annuler',
+              customId: 'cancel',
+              style: 'DANGER'
+            })
+          ]
+        }
+      ],
+      ephemeral: true,
+      fetchReply: true
+    })) as Message;
+
+    return new Promise((resolve) => {
+      const collector = message.createMessageComponentCollector({
+        max: 1,
+        filter: (i: MessageComponentInteraction) =>
+          i.user.id === interaction.user.id
+      });
+      collector.once(
+        'end',
+        async (
+          interactions: Collection<string, ButtonInteraction>,
+          reason: string
+        ) => {
+          const buttonInteraction = interactions.first();
+          if (!buttonInteraction) {
+            if (reason !== 'time') resolve(null);
+            if (message.deletable) await message.delete();
+            await interaction.reply({
+              content: 'Les 60 secondes sont écoulées',
+              ephemeral: true
+            });
+            return resolve(null);
+          }
+
+          return resolve(buttonInteraction);
+        }
+      );
+    });
   }
 }
