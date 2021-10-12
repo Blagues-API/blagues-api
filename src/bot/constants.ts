bfeb390a9796898e562c3eec1bfcfda2b44395fc
DraftProducts <nicovanaarsen@gmail.com>
2021-10-12 20:12:40 +0200

    refactor: ♻️ Big refactor of constants with dotenv values

--- src/bot/constants.ts
+++ src/bot/constants.ts
@@ -1,16 +1,14 @@
 import { Snowflake } from 'discord-api-types';
 
-export enum AdminUsers {
-  DraftMan = '207190782673813504',
-  Saphirr = '207190782673813504',
-  Panda = '164738865649811457',
-  Esther = '655032713941614632',
-  Erwan = '150249602635792385'
-}
-
 export const jokeRole: Snowflake = '699244416849674310';
 export const parrainRole: Snowflake = '877511831525154837';
-export const everyoneRole: Snowflake = '877545109426683904';
-export const suggestsChannel: Snowflake = '884077276210290789';
-export const correctionChannel: Snowflake = '875680971683414026';
+
+export const suggestsChannel: Snowflake =
+  process.env.SUGGESTION_CHANNEL ?? '698826767221391390';
+export const correctionChannel: Snowflake =
+  process.env.CORRECTION_CHANNEL ?? '826856142793736213';
+
+export const upReaction: Snowflake = '705115420495183979';
+export const downReaction: Snowflake = '705115406976680117';
+
 export const guildId: Snowflake = '698822532467523605';
