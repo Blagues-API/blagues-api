5ceb337f2995426b52b7cb47d86826eef54cabb4
DraftProducts <nicovanaarsen@gmail.com>
2021-11-16 23:50:38 +0100

    feat: ✨ Add utils generation

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,35 @@
+import dayjs from 'dayjs';
+import { Snowflake } from 'discord-api-types';
+import jwt from 'jsonwebtoken';
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const random = (items: any[]): any => {
   return items[Math.floor(Math.random() * items.length)];
+};
+
+export const generateAPIToken = (
+  userId: Snowflake,
+  key: string,
+  limit: number
+) => {
+  return jwt.sign(
+    {
+      user_id: userId,
+      limit,
+      key,
+      created_at: dayjs().format()
+    },
+    process.env.JWT_TOKEN!
+  );
+};
+
+export const generateKey = () => {
+  let result = '';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 50; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 };
