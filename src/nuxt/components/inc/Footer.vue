db033e56eebb3a4cbb19f538185c331d7f970cc0
DraftProducts <nicovanaarsen@gmail.com>
2021-11-18 16:37:46 +0100

    fix: 🐛 Fix footer Design

--- src/nuxt/components/inc/Footer.vue
+++ src/nuxt/components/inc/Footer.vue
@@ -1,13 +1,19 @@
 <template>
   <footer>
     <div class="copyright">
-      Blagues API © 2019-2021
-      <a href="https://www.draftman.fr" title="DraftMan Developer">DraftMan</a>
-      | Powered by
-      <a
-        href="https://www.draftbot.fr"
-        title="DraftBot Discord Bot"
-      >DraftBot</a>
+      <div>
+        Blagues API © 2019-{{ new Date().getFullYear() }}
+        <a href="https://www.draftman.fr" title="DraftMan Developer"
+          >DraftMan</a
+        >
+      </div>
+      <span class="separator">|</span>
+      <div>
+        Powered by
+        <a href="https://www.draftbot.fr" title="DraftBot Discord Bot"
+          >DraftBot</a
+        >
+      </div>
     </div>
   </footer>
 </template>
@@ -19,14 +25,27 @@ footer {
   align-items: center;
   background-color: var(--secondary);
   color: var(--white);
-  height: 40px;
+  min-height: 40px;
   .copyright {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     color: #ffffff;
     font-weight: bold;
     font-size: 14px;
     line-height: 22px;
+    padding: 8px 0;
     a {
       text-decoration: none;
+    }
+    .separator {
+      margin: 0 8px;
+    }
+    @media screen and(max-width: 720px) {
+      flex-direction: column;
+      .separator {
+        display: none;
+      }
     }
   }
 }
