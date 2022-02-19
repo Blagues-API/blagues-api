4f55f386cecd5e9e040117ef4b8c9842c7be62e4
DraftMan <nicovanaarsen@gmail.com>
2022-02-19 22:47:44 +0100

    refactor: ♻️ Refactor of stats command

--- src/bot/commands/stats.ts
+++ src/bot/commands/stats.ts
@@ -29,76 +29,84 @@ export default class SuggestCommand extends Command {
     }
 
     if (interaction.options.get('user')) {
-      const member = interaction.options.getMember('user') as GuildMember;
-      if (!member) return interaction.reply(interactionProblem("Cet utilisateur n'est plus présent sur le serveur."));
+      return this.userStats(interaction);
+    }
 
-      const fields = [];
-      const proposals = await prisma.proposal.findMany({
-        where: {
-          user_id: member.id,
-          stale: false
-        }
+    return this.globalStats(interaction);
+  }
+
+  async userStats(interaction: CommandInteraction) {
+    const member = interaction.options.getMember('user') as GuildMember;
+    if (!member) return interaction.reply(interactionProblem("Cet utilisateur n'est plus présent sur le serveur."));
+
+    const fields = [];
+    const proposals = await prisma.proposal.findMany({
+      where: {
+        user_id: member.id,
+        stale: false
+      }
+    });
+
+    const [suggestions, corrections] = partition(proposals, (proposal) => proposal.type === ProposalType.SUGGESTION);
+
+    fields.push({
+      name: 'Statistiques globales',
+      value: stripIndents`
+        Blagues proposées: **${suggestions.length}**
+        Blagues en attente: **${suggestions.filter((s) => !s.refused && !s.merged).length}**
+        Blagues acceptées: **${suggestions.filter((s) => s.merged).length}**
+
+        Corrections proposées: **${corrections.length}**
+        Corrections en attente: **${corrections.filter((c) => !c.refused && !c.merged).length}**
+        Corrections acceptées: **${corrections.filter((c) => c.merged).length}**
+      `
+    });
+
+    if (member.roles.cache.has(parrainRole)) {
+      const approvals = await prisma.approval.findMany({
+        where: { user_id: member.id },
+        include: { proposal: true }
+      });
+      const disapprovals = await prisma.disapproval.findMany({
+        where: { user_id: member.id },
+        include: { proposal: true }
       });
 
-      const [suggestions, corrections] = partition(proposals, (proposal) => proposal.type === ProposalType.SUGGESTION);
+      const totalDecisionsCount = approvals.length + disapprovals.length;
+      const suggestsDecisionsCount = [...approvals, ...disapprovals].filter(
+        (approval) => approval.proposal.type === ProposalType.SUGGESTION
+      ).length;
 
       fields.push({
-        name: 'Statistiques globales',
+        name: 'Décisions de Parrain',
         value: stripIndents`
-          Blagues proposées: **${suggestions.length}**
-          Blagues en attente: **${suggestions.filter((s) => !s.refused && !s.merged).length}**
-          Blagues acceptées: **${suggestions.filter((s) => s.merged).length}**
+          Décisions: **${totalDecisionsCount}**
 
-          Corrections proposées: **${corrections.length}**
-          Corrections en attente: **${corrections.filter((c) => !c.refused && !c.merged).length}**
-          Corrections acceptées: **${corrections.filter((c) => c.merged).length}**
+          Blagues: **${suggestsDecisionsCount}**
+          Corrections: **${totalDecisionsCount - suggestsDecisionsCount}**
         `
-      });
-
-      if (member.roles.cache.has(parrainRole)) {
-        const approvals = await prisma.approval.findMany({
-          where: { user_id: member.id },
-          include: { proposal: true }
-        });
-        const disapprovals = await prisma.disapproval.findMany({
-          where: { user_id: member.id },
-          include: { proposal: true }
-        });
-
-        const totalDecisionsCount = approvals.length + disapprovals.length;
-        const suggestsDecisionsCount = [...approvals, ...disapprovals].filter(
-          (approval) => approval.proposal.type === ProposalType.SUGGESTION
-        ).length;
-
-        fields.push({
-          name: 'Décisions de Parrain',
-          value: stripIndents`
-            Décisions: **${totalDecisionsCount}**
-
-            Blagues: **${suggestsDecisionsCount}**
-            Corrections: **${totalDecisionsCount - suggestsDecisionsCount}**
-          `
-        });
-      }
-
-      return interaction.reply({
-        embeds: [
-          {
-            author: {
-              icon_url: member.displayAvatarURL({ dynamic: true, size: 32 }),
-              name: `Statistiques de ${member.displayName}`
-            },
-            fields,
-            color: Colors.PRIMARY,
-            footer: {
-              text: 'Blagues API',
-              icon_url: interaction.guild!.iconURL({ size: 32 }) ?? undefined
-            }
-          }
-        ]
       });
     }
 
+    return interaction.reply({
+      embeds: [
+        {
+          author: {
+            icon_url: member.displayAvatarURL({ dynamic: true, size: 32 }),
+            name: `Statistiques de ${member.displayName}`
+          },
+          fields,
+          color: Colors.PRIMARY,
+          footer: {
+            text: 'Blagues API',
+            icon_url: interaction.guild!.iconURL({ size: 32 }) ?? undefined
+          }
+        }
+      ]
+    });
+  }
+
+  async globalStats(interaction: CommandInteraction) {
     // Issue reported: https://github.com/prisma/prisma/issues/10915
     const proposals = await prisma.proposal.groupBy({
       by: ['user_id', 'merged'],
@@ -115,7 +123,7 @@ export default class SuggestCommand extends Command {
       embeds: [
         {
           title: 'Statistiques',
-          description: proposals
+          description: [...proposals]
             .sort((a, b) => b._count - a._count)
             .map(
               (proposal) => `<@${proposal.user_id}> : ${proposal._count} ${proposal._count !== 1 ? 'points' : 'point'}`
