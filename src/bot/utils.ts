6c0642728c0c0cdd33c905cb7bf775e7e07628cb
Williams Bussat <w.bussat@icloud.com>
2022-06-21 01:14:53 +0200

    feat: ✨ Add fonction for delete message

--- src/bot/utils.ts
+++ src/bot/utils.ts
@@ -1,4 +1,4 @@
-import { InteractionReplyOptions, TextChannel } from 'discord.js';
+import { InteractionReplyOptions, Message, TextChannel } from 'discord.js';
 import { diffWords } from 'diff';
 import { APIEmbed } from 'discord-api-types/v10';
 
@@ -75,4 +75,8 @@ export function showNegativeDiffs(oldValue: string, newValue: string): string {
 export function isEmbedable(channel: TextChannel) {
   const permissions = channel.permissionsFor(channel.guild.members.me!);
   return permissions?.has(['ViewChannel', 'SendMessages', 'EmbedLinks']);
+}
+
+export function tDelete(timeout = 6000) {
+  return (message: Message) => setTimeout(() => message.deletable && message.delete().catch(() => null), timeout);
 }
