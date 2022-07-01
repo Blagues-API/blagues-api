6dc2df6d6586e299eb4ab3c7817b7f9b496db135
Mr. PRAINGLE <maelgronier@protonmail.com>
2022-07-02 00:21:13 +0200

    feat: ✨ Update emoji for godfather avatars

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,6 +2,7 @@ import {
   ActivityType,
   AnyInteraction,
   Client,
+  GuildMember,
   GuildTextBasedChannel,
   IntentsBitField,
   InteractionType,
@@ -11,15 +12,17 @@ import {
 } from 'discord.js';
 import Jokes from '../jokes';
 import prisma from '../prisma';
-import { correctionsChannelId, suggestionsChannelId } from './constants';
+import { correctionsChannelId, suggestionsChannelId, emojisGuildId, godfatherRoleId } from './constants';
 import Dispatcher from './lib/dispatcher';
 import Reminders from './modules/reminders';
 import Stickys from './modules/stickys';
+import { getGodfatherEmoji } from './modules/godfathers';
+import sharp from 'sharp';
+import got from 'got';
 export default class Bot extends Client {
   public dispatcher: Dispatcher;
   public stickys: Stickys;
   public reminders: Reminders;
-
   constructor() {
     super({
       partials: [Partials.Reaction],
@@ -96,12 +99,46 @@ export default class Bot extends Client {
   async onMessageCreate(message: Message | PartialMessage): Promise<void> {
     if (!message.inGuild()) return;
     this.stickys.run(message);
+  } // dis moi je chauffe ou je refroidis
+
+  async onGuildMemberUpdate(oldMember: GuildMember, newMember: GuildMember) {
+    if (oldMember.displayAvatarURL() !== newMember.displayAvatarURL()) {
+      if (!newMember.roles.cache.has(godfatherRoleId)) return;
+      const guild = this.guilds.cache.get(emojisGuildId);
+      const emoji = getGodfatherEmoji(emojisGuildId, oldMember).emoji;
+      if (emoji! in guild) {
+        return console.log(`L'émoji de parrain de ${newMember.displayName} n'est pas présent sur ${guild.name} !`);
+      }
+      const rect = Buffer.from('<svg><rect x="0" y="0" width="128" height="128" rx="64" ry="64"/></svg>');
+      const bufferAvatar = await got(
+        newMember.displayAvatarURL({ size: 128, forceStatic: true, extension: 'png' })
+      ).buffer();
+      const bufferEmoji = await sharp(bufferAvatar)
+        .composite([{ input: rect, blend: 'dest-in' }])
+        .toBuffer();
+      const newEmoji = await guild.emojis.create({
+        name: 'vote',
+        attachment: bufferEmoji,
+        roles: ['698914163677724753', '969717353963225191']
+      });
+      await prisma.godfather.update({
+        where: {
+          user_id: newMember.id
+        },
+        data: {
+          emoji_id: newEmoji.id
+        }
+      });
+    } else {
+      return;
+    }
   }
 
   registerEvents(): void {
     this.on('interactionCreate', this.onInteractionCreate.bind(this));
     this.on('messageDelete', this.onMessageDelete.bind(this));
     this.on('messageCreate', this.onMessageCreate.bind(this));
+    this.on('guildMemberUpdate', this.onGuildMemberUpdate.bind(this));
   }
 
   refreshStatus() {
