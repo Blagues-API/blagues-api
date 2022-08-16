efeff8fe1951f97d497bd1c297ea520f0eba6dcd
Swiizyy <williams@wbussat.fr>
2022-08-16 11:43:51 +0200

    fix: :bug: Fix from review
    Signed-off-by: Swiizyy <williams@wbussat.fr>

--- src/bot/utils/embeds.ts
+++ src/bot/utils/embeds.ts
@@ -1,10 +1,31 @@
 import { APIEmbed } from 'discord-api-types/v10';
 import { Colors } from '../constants';
+import { InteractionReplyOptions, MessageOptions } from 'discord.js';
 
-export function problem(message: string): APIEmbed {
+type UniversalMessageOptions = Omit<MessageOptions, 'flags'>;
+type UniversalInteractionOptions = Omit<InteractionReplyOptions, 'flags'>;
+
+export function validate(message: string): APIEmbed {
   return {
-    description: `❌ ${message}`,
-    color: Colors.DANGER
+    description: `✅ ${message}`,
+    color: Colors.SUCCESS
+  };
+}
+
+export function messageValidate(message: string): UniversalMessageOptions {
+  return {
+    content: '',
+    embeds: [validate(message)],
+    components: []
+  };
+}
+
+export function interactionValidate(message: string, ephemeral = true): UniversalInteractionOptions {
+  return {
+    content: '',
+    embeds: [validate(message)],
+    components: [],
+    ephemeral
   };
 }
 
@@ -15,9 +36,43 @@ export function info(message: string): APIEmbed {
   };
 }
 
-export function validate(message: string): APIEmbed {
+export function messageInfo(message: string): UniversalMessageOptions {
   return {
-    description: `✅ ${message}`,
-    color: Colors.SUCCESS
+    content: '',
+    embeds: [info(message)],
+    components: []
+  };
+}
+
+export function interactionInfo(message: string, ephemeral = true): UniversalInteractionOptions {
+  return {
+    content: '',
+    embeds: [info(message)],
+    components: [],
+    ephemeral
+  };
+}
+
+export function problem(message: string): APIEmbed {
+  return {
+    description: `❌ ${message}`,
+    color: Colors.DANGER
+  };
+}
+
+export function messageProblem(message: string): UniversalMessageOptions {
+  return {
+    content: '',
+    embeds: [problem(message)],
+    components: []
+  };
+}
+
+export function interactionProblem(message: string, ephemeral = true): UniversalInteractionOptions {
+  return {
+    content: '',
+    embeds: [problem(message)],
+    components: [],
+    ephemeral
   };
 }
--- src/bot/utils/interactions.ts
+++ src/bot/utils/interactions.ts
@@ -1,14 +1,11 @@
 import {
   ButtonInteraction,
-  ButtonStyle,
-  CommandInteraction,
   ComponentType,
   Message,
   MessageComponentType,
   SelectMenuInteraction,
   User
 } from 'discord.js';
-import { APIEmbed } from 'discord-api-types/v10';
 import { interactionInfo } from './embeds';
 
 type WaitForInteractionOptions<T extends MessageComponentType> = {
@@ -40,61 +37,4 @@ export async function waitForInteraction<T extends WaitForInteractionOptions<Mes
         if (reason !== 'idle') reject(reason);
       });
   });
-}
-
-export async function paginate(
-  interaction: CommandInteraction<'cached'>,
-  embed: APIEmbed,
-  pages: string[],
-  page = 0,
-  oldMessage: Message<true> | null = null
-): Promise<void> {
-  const message =
-    oldMessage ||
-    (await interaction.reply({
-      embeds: [embed],
-      components:
-        pages.length > 1
-          ? [
-              {
-                type: ComponentType.ActionRow,
-                components: [
-                  { type: ComponentType.Button, label: 'Précedent', style: ButtonStyle.Primary, customId: 'last' },
-                  { type: ComponentType.Button, label: 'Suivant', style: ButtonStyle.Primary, customId: 'next' }
-                ]
-              }
-            ]
-          : [],
-      fetchReply: true
-    }));
-
-  if (pages.length <= 1) return;
-
-  try {
-    const buttonInteraction = await waitForInteraction({
-      component_type: ComponentType.Button,
-      message: message,
-      user: interaction.user
-    });
-
-    if (!buttonInteraction) return;
-
-    switch (buttonInteraction.customId) {
-      case 'last':
-        page = (page > 0 ? page : pages.length) - 1;
-        break;
-      case 'next':
-        page = page < pages.length - 1 ? page + 1 : 0;
-        break;
-    }
-
-    embed.description = pages[page];
-    embed.footer = { ...(embed.footer ?? {}), text: `Page ${page + 1}/${pages.length} • Blagues-API` };
-
-    await buttonInteraction.update({ embeds: [embed] });
-  } catch (error) {
-    // TODO: Catch les erreurs
-  }
-
-  return paginate(interaction, embed, pages, page, message);
 }
