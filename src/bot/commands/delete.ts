df6bb638f86d5b736e13a7338990ebbcde146e9b
MrPRAINGLE <maelgronier@protonmail.com>
2022-09-01 00:35:42 +0200

    feat: ✨ Addition of delete confirmation

--- src/bot/commands/delete.ts
+++ src/bot/commands/delete.ts
@@ -1,11 +1,18 @@
 import Command from '../lib/command';
 import {
+  APIEmbed,
   ApplicationCommandOptionType,
   ApplicationCommandType,
+  ButtonStyle,
   ChatInputCommandInteraction,
+  ComponentType,
+  Message,
   roleMention
 } from 'discord.js';
-import { interactionInfo } from '../utils';
+import { buildJokeDisplay, interactionInfo, waitForInteraction } from '../utils';
+import { CategoriesRefs } from '../../typings';
+import { jokeById, jokeByQuestion } from '../../controllers';
+import { Colors } from '../constants';
 
 export default class DeleteCommand extends Command {
   constructor() {
@@ -29,6 +36,62 @@ export default class DeleteCommand extends Command {
       await interaction.reply(
         interactionInfo(`Seul le ${roleMention('698914163677724753')} du bot peut exécuter cette commande.`)
       );
+    }
+
+    const query = interaction.options.getString('recherche', true);
+    const joke = isNaN(Number(query)) ? jokeByQuestion(query)! : jokeById(Number(query))!;
+
+    const embed: APIEmbed = {
+      author: {
+        icon_url: interaction.user.displayAvatarURL({
+          size: 32
+        }),
+        name: interaction.user.tag
+      },
+      description: buildJokeDisplay(CategoriesRefs[joke.type], joke.joke, joke.answer),
+      color: Colors.PROPOSED
+    };
+
+    const message = (await interaction.reply({
+      content: 'Confirmez-vous vouloir supprimer la blague suivante ?',
+      embeds: [embed],
+      components: [
+        {
+          type: ComponentType.ActionRow,
+          components: [
+            {
+              type: ComponentType.Button,
+              label: 'Supprimer',
+              customId: 'delete',
+              style: ButtonStyle.Danger
+            },
+            {
+              type: ComponentType.Button,
+              label: 'Annuler',
+              customId: 'cancel',
+              style: ButtonStyle.Secondary
+            }
+          ]
+        }
+      ],
+      ephemeral: true,
+      fetchReply: true
+    })) as Message<true>;
+
+    const confirmation = await waitForInteraction({
+      component_type: ComponentType.Button,
+      message: message,
+      user: interaction.user
+    });
+
+    if (!confirmation) return;
+
+    if (confirmation.customId === 'cancel') {
+      return confirmation.update({
+        content: "La blague n'a pas été envoyée.",
+        components: [],
+        embeds: [embed]
+      });
     }
   }
 }
